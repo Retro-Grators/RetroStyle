8960e9ecda9916460af4d785b9e40cc65b513a4e
Cole Frishman <awesomeeloc@gmail.com>
2021-01-30 13:17:17 -0500

    parses object into a theme

--- extension-src/popup.js
+++ extension-src/popup.js
@@ -1,13 +1,50 @@
 var tab;
+var darkMode={
+	font:"",
+	fontColor:"white",
+	headingFont:"",
+	headingColor:"#dddddd",
+	backgroundColor:"black",
 
-function changeTheme (theme) {
-	//document.getElementsByTagName('body')[0].style.setProperty('background-color','black')
-	Array.prototype.forEach.call(document.getElementsByTagName('*'), element => {
-		element.style.setProperty('background-color','black');
-	})
-	Array.prototype.forEach.call(document.getElementsByTagName('*'), element => {
-		element.style.setProperty('color','white');
-	})
+};
+var th = darkMode;
+
+function changeTheme () {
+	function setStyles(element){
+		const headerTags = ["H1","H2","H3","H4", "H5", "H6"]
+		Array.prototype.forEach.call(document.getElementsByTagName('*'), element => {
+			if(theme.font){
+				element.style.setProperty('font-family',theme.font);
+			}
+			if(theme.fontColor){
+				element.style.setProperty('color',theme.fontColor);
+			}
+			if(theme.backgroundColor){
+				element.style.setProperty('background-color',theme.backgroundColor);
+			}
+			if(theme.headingFont && headerTags.includes(element.tagName)){
+				element.style.setProperty('font-family',theme.headingFont);
+			}
+			if(theme.headingColor && headerTags.includes(element.tagName)){
+				element.style.setProperty('color',theme.headingColor);
+			}
+		});
+	}
+
+	setStyles();
+
+	
+	setTimeout(() => {
+		setStyles();
+	}, 500)
+}
+
+function getThemeChanger(tab, theme){
+	let funcStr = changeTheme.toString();
+	//The code after 'code:' is run with the execute script, but it needs to be in string form 
+	chrome.tabs.executeScript(tab.id, {code:`var theme=${JSON.stringify(theme)};`}, function() {
+		chrome.tabs.executeScript(tab.id, {code:`${funcStr.slice(funcStr.indexOf('{')+1, funcStr.lastIndexOf('}'))}`});
+	});
 }
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -17,9 +54,7 @@ document.addEventListener('DOMContentLoaded', function () {
 		chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
 			tab = tabs[0]
 
-			//The code after 'code:' is run with the execute script, but it needs to be in string form 
-			chrome.tabs.executeScript(tab.id, {code:`${changeTheme.toString().slice(32, changeTheme.toString().length-2)}`});
-
+			getThemeChanger(tab, th)
 		})
 	})
 });
