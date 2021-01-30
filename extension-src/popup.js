9cfdbd5558bd734c393b80e6d947ca7d1a5ba412
Cole Frishman <awesomeeloc@gmail.com>
2021-01-30 10:37:30 -0500

    proof-of-concept for changing the theme

--- extension-src/popup.js
+++ extension-src/popup.js
@@ -1,5 +1,22 @@
+var tab;
+
+function changeTheme (theme) {
+	document.getElementsByTagName('body')[0].style.setProperty('background-color','black')
+	Array.prototype.forEach.call(document.getElementsByTagName('*'), element => {
+		element.style.setProperty('color','white');
+	})
+}
+
 document.addEventListener('DOMContentLoaded', function () {
+	//onclick for the button
 	document.getElementById('btn').addEventListener('click', () => {
-		alert(3)
+		//get the current tab
+		chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+			tab = tabs[0]
+
+			//The code after 'code:' is run with the execute script, but it needs to be in string form 
+			chrome.tabs.executeScript(tab.id, {code:`${changeTheme.toString().slice(32, changeTheme.toString().length-3)}`});
+		})
 	})
 });
+
